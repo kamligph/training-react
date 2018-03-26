d9703faf89a1d7b253c79970e0c74b75d74e6403
Paul James Curacha <paul_james_curacha@ligph.com>
2018-03-26 19:12:05 +0800

    Add dummy states

--- src/App.js
+++ src/App.js
@@ -3,11 +3,30 @@ import Projects from './Component/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce Cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
         Training React App
-        <Projects data="awesome-ness" />
+        <Projects data="awesome-ness" projects={this.state.projects} />
       </div>
     );
   }
