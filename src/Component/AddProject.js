1a88092e46ffb81b725610f5018da13de50373f2
Paul James Curacha <paul_james_curacha@ligph.com>
2018-03-28 17:42:18 +0800

    Modify Projects.js and AddProject.js markup

--- src/Component/AddProject.js
+++ src/Component/AddProject.js
@@ -34,7 +34,7 @@ class AddProject extends Component {
 
   render() {
     let categoryOptions = this.props.categories.map(category => {
-        return <option key={category} value="category">{category}</option>
+        return <option key={category} value={category}>{category}</option>
       }
     )
     return (
@@ -43,15 +43,16 @@ class AddProject extends Component {
 
         <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
-            <label>Title</label>
+            <label>Title</label><br/>
             <input type="text" ref="title" />
           </div>
           <div>
-            <label>Category</label>
+            <label>Category</label><br/>
             <select ref="category">
               {categoryOptions}
             </select>
           </div>
+          <br/>
           <input type="submit" value="Submit" />
         </form>
       </div>
